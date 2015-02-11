9e3fdf099f0d3c36a4e730ba18dea9a66fa96956
Colin But <colin.but@bjss.com>
2015-02-11 11:16:37 +0000

    added HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * Java Hello World
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+
+	}
+
+}
